1c0c641037f6cb20b0de0c5bc5729f3dcd8d0c9b
NelsonNV <nelsonluisnv2@gmail.com>
2024-10-12 19:39:52 -0300

    feat: changed url media

--- files/urls.py
+++ files/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", FileManagerView.as_view(), name="file_manager"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
